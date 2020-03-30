94065517c2cfbdc2974c419b3d563ecb1c7baf65
hanszhou80 <hanszhou80@gmail.com>
2020-03-30 14:27:51 +0800

    更改prod配置

--- src/main/java/com/xinbo/cloud/task/statistics/consumer/CloudStatisticsConsumer.java
+++ src/main/java/com/xinbo/cloud/task/statistics/consumer/CloudStatisticsConsumer.java
@@ -3,11 +3,8 @@ package com.xinbo.cloud.task.statistics.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@EnableScheduling
-@EnableSwagger2
 @MapperScan(basePackages = "com.xinbo.cloud")
 @SpringBootApplication(scanBasePackages = "com.xinbo.cloud")
 public class CloudStatisticsConsumer {
